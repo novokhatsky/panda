0af4badf7652734622f9fd0ca95600f88df7acfa
novokhatsky <program_maker@mail.ru>
2022-07-18 23:17:31 +0700

    отработка движения вправо/влево

--- js/main.js
+++ js/main.js
@@ -63,13 +63,58 @@ window.onload = function () {
     }
 
     function square() {
-        tetris[0][0] = 1 + Math.floor(Math.random() * MAX_COLOR);
+        //tetris[0][0] = 1 + Math.floor(Math.random() * MAX_COLOR);
+        
+        return {x: 0, y: 0, color: 1 + Math.floor(Math.random() * MAX_COLOR)};
     }
 
     function down() {
-        draw();
+        timer = setTimeout(function () {
+            let newDrop = true;
 
-        let newDrop = true;
+            tetris[figure.y][figure.x] = figure.color;
+
+            draw();
+
+            // move down
+            if (figure.y < HEIGHT - 1) {
+                if (tetris[figure.y + 1][figure.x] == 0) {
+                    tetris[figure.y][figure.x] = 0;
+                    figure.y += 1;
+                    newDrop = false;
+                }
+            }
+
+            if (newDrop) {
+                // проверка заполненных линий
+
+                figure = square();
+            }
+
+            down();
+
+        }, 500);
+    }
+
+    function moveRight() {
+        if (figure.x < WIDTH - 1) {
+            if (tetris[figure.y][figure.x + 1] == 0) {
+                tetris[figure.y][figure.x] = 0;
+                figure.x += 1
+            }
+        }
+    }
+
+    function moveLeft() {
+        if (figure.x > 0) {
+            if (tetris[figure.y][figure.x - 1] == 0) {
+                tetris[figure.y][figure.x] = 0;
+                figure.x -= 1
+            }
+        }
+    }
+
+    function scrollDown() {
 
         for (let i = HEIGHT - 2; i >= 0; i--) {
             for (let j = 0; j < WIDTH; j++) {
@@ -80,15 +125,25 @@ window.onload = function () {
                 }
             }
         }
-
-        if (newDrop) {
-            square();
-        }
     }
 
     let tetris = init();
     draw();
-    square();
+    let figure = square();
 
     document.querySelector('.start').onclick = down;
+    document.onkeydown = function (event) {
+
+        switch (event.code) {
+            case 'ArrowRight':
+                moveRight();
+                break;
+
+            case 'ArrowLeft':
+                moveLeft();
+                break;
+        }
+
+        return false;
+    }
 }
